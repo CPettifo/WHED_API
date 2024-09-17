5e4f5b4710a4209c39edf9b8eb02ce6c9ade7936
CPettifo <craig_petti4@hotmail.com>
2024-09-17 12:16:33 +0200

    start testing local mysql connection

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -1,6 +1,6 @@
 using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-using System.Data;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace WhedNamespace
@@ -8,34 +8,46 @@ namespace WhedNamespace
     public class DatabaseContext
     {
         private readonly string _connectionString;
+        private readonly ILogger<DatabaseContext> _logger;
 
-        public DatabaseContext(string connectionString)
+        public DatabaseContext(string connectionString, ILogger<DatabaseContext> logger)
         {
             _connectionString = connectionString;
+            _logger = logger;
         }
 
-        public async Task<List<string>> GetTestDataAsync()
+        public async Task<List<string>> GetSingleColumnAsync()
         {
             List<string> data = new List<string>();
 
-            using (MySqlConnection conn = new MySqlConnection(_connectionString))
-            {
-                await conn.OpenAsync();
-
-                using (MySqlCommand cmd = new MySqlCommand("SELECT OrgID, OrgName FROM whed_org", conn))
+            try{
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
                 {
-                    using (var reader = await cmd.ExecuteReaderAsync())
+                    await conn.OpenAsync();
+                    _logger.LogInformation("MySQL Connection Successful");
+
+                    // Modified SQL query with CAST for safety
+                    using (MySqlCommand cmd = new MySqlCommand("SELECT CAST(OrgID AS CHAR) AS OrgID, CAST(OrgName AS CHAR) AS OrgName FROM whed_org", conn))
                     {
-                        while (await reader.ReadAsync())
+                        using (var reader = await cmd.ExecuteReaderAsync())
                         {
-                            data.Add(reader.GetString("OrgID"));
-                            data.Add(reader.GetString("OrgName"));
+                            while (await reader.ReadAsync())
+                            {
+                                // Using GetOrdinal to ensure correct indexing
+                                data.Add(reader.GetString(reader.GetOrdinal("OrgID")));
+                                data.Add(reader.GetString(reader.GetOrdinal("OrgName")));
+                            }
                         }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error with connection: " + ex.Message);
+            }
 
             return data;
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
+using Microsoft.VisualBasic;
 using WhedNamespace;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+
 // Add services to the container
 builder.Services.AddSingleton<DatabaseContext>(provider =>
-    new DatabaseContext(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+{
+    var logger = provider.GetRequiredService<ILogger<DatabaseContext>>();
+    return new DatabaseContext(builder.Configuration.GetConnectionString("DefaultConnection"), logger);
+});
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 
 
@@ -16,10 +22,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WHED API");
+    });
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
